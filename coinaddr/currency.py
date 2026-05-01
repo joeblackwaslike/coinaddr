e48dc083ca6cfe5c9da91f63cec5e8b5ece1c3be
Joe Black <me@joeblack.nyc>
2026-05-01 18:53:09 -0400

    chore: update currency module and add workspace file

--- coinaddr/currency.py
+++ coinaddr/currency.py
@@ -39,52 +39,77 @@ class CurrencyMeta(type):
 class Currency(metaclass=CurrencyMeta):
     """An immutable representation of a cryptocurrency specification."""
 
-    name = attr.ib(
-        type=str,
-        validator=attr.validators.instance_of(str))
-    ticker = attr.ib(
-        type=str,
-        validator=attr.validators.instance_of(str))
-    validator = attr.ib(
-        type=str,        
-        validator=attr.validators.instance_of(str))
+    name = attr.ib(type=str, validator=attr.validators.instance_of(str))
+    ticker = attr.ib(type=str, validator=attr.validators.instance_of(str))
+    validator = attr.ib(type=str, validator=attr.validators.instance_of(str))
     networks = attr.ib(
         type=dict,
         validator=attr.validators.optional(attr.validators.instance_of(dict)),
-        default=attr.Factory(dict))
+        default=attr.Factory(dict),
+    )
     charset = attr.ib(
         type=bytes,
         validator=attr.validators.optional(attr.validators.instance_of(bytes)),
-        default=None)
+        default=None,
+    )
 
 
-Currency('bitcoin', ticker='btc', validator='Base58Check',
-         networks=dict(
-             main=(0x00, 0x05), test=(0x6f, 0xc4)))
-Currency('bitcoin-cash', ticker='bch', validator='Base58Check',
-         networks=dict(
-             main=(0x00, 0x05), test=(0x6f, 0xc4, 0x3a)))
-Currency('litecoin', ticker='ltc', validator='Base58Check',
-         networks=dict(
-             main=(0x30, 0x05, 0x32), test=(0x6f, 0xc4, 0x3a)))
-Currency('dogecoin', ticker='doge', validator='Base58Check',
-         networks=dict(
-             main=(0x1e, 0x16), test=(0x71, 0xc4)))
-Currency('dashcoin', ticker='dash', validator='Base58Check',
-         networks=dict(
-             main=(0x4c, 0x10), test=(0x8c, 0x13)))
-Currency('neocoin', ticker='neo', validator='Base58Check',
-         networks=dict(both=(0x17,)))
-Currency('ripple', ticker='xrp', validator='Base58Check',
-         networks=dict(both=(0x00, 0x05)),
-         charset=(b'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcd'
-                  b'eCg65jkm8oFqi1tuvAxyz'))
-Currency('ethereum', ticker='eth', validator='Ethereum')
-Currency('ether-zero', ticker='etz', validator='Ethereum')
-Currency('ethereum-classic', ticker='etc', validator='Ethereum')
-Currency('bitcoin-segwit', ticker='btc-segwit', validator='SegWitCheck',
-         networks=dict(
-             main=('bc', ), test=('tb', )))
-Currency('litecoin-segwit', ticker='ltc-segwit', validator='SegWitCheck',
-         networks=dict(
-             main=('ltc', ), test=('tltc', )))
+Currency(
+    "bitcoin",
+    ticker="btc",
+    validator="Base58Check",
+    networks=dict(main=(0x00, 0x05), test=(0x6F, 0xC4)),
+)
+Currency(
+    "bitcoin-cash",
+    ticker="bch",
+    validator="Base58Check",
+    networks=dict(main=(0x00, 0x05), test=(0x6F, 0xC4, 0x3A)),
+)
+Currency(
+    "litecoin",
+    ticker="ltc",
+    validator="Base58Check",
+    networks=dict(main=(0x30, 0x05, 0x32), test=(0x6F, 0xC4, 0x3A)),
+)
+Currency(
+    "dogecoin",
+    ticker="doge",
+    validator="Base58Check",
+    networks=dict(main=(0x1E, 0x16), test=(0x71, 0xC4)),
+)
+Currency(
+    "dashcoin",
+    ticker="dash",
+    validator="Base58Check",
+    networks=dict(main=(0x4C, 0x10), test=(0x8C, 0x13)),
+)
+Currency(
+    "dogecoin",
+    ticker="doge",
+    validator="Base58Check",
+    networks=dict(main=(0x1E, 0x16), test=(0x71, 0xC4)),
+)
+Currency("neocoin", ticker="neo", validator="Base58Check", networks=dict(both=(0x17,)))
+Currency(
+    "ripple",
+    ticker="xrp",
+    validator="Base58Check",
+    networks=dict(both=(0x00, 0x05)),
+    charset=(b"rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz"),
+)
+Currency("ethereum", ticker="eth", validator="Ethereum")
+Currency("ether-zero", ticker="etz", validator="Ethereum")
+Currency("ethereum-classic", ticker="etc", validator="Ethereum")
+Currency(
+    "bitcoin-segwit",
+    ticker="btc-segwit",
+    validator="SegWitCheck",
+    networks=dict(main=("bc",), test=("tb",)),
+)
+Currency(
+    "litecoin-segwit",
+    ticker="ltc-segwit",
+    validator="SegWitCheck",
+    networks=dict(main=("ltc",), test=("tltc",)),
+)
